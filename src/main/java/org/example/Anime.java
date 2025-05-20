3c433ac25c7f22ed7532e220838824b09da8f86d
IagoRibLima <iagolima40028922@gmail.com>
2025-05-19 21:42:56 -0300

    iniciando projeto

---
+++ src/main/java/org/example/Anime.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import java.util.Scanner;
+import java.text.Normalizer;
+
+public class Anime {
+    //Cadastro dos animes
+    public String nome;
+    public String comentario;
+    public int nota;
+    public int numero;
+
+    //Construtor para acessar a classe
+    public Anime(String nome, String comentario, int nota, int numero) {
+        this.nome = nome;
+        this.comentario = comentario;
+        this.nota = nota;
+        this.numero = numero;
+    }
+
+    //Métodos
+    public static Anime cadastrarAnime(Scanner sc) {
+        System.out.print("Nome: ");
+        sc.nextLine(); //Limpa Buffer
+        String nome = sc.nextLine();
+
+        System.out.print("Comentário: ");
+        String comentario = sc.nextLine();
+
+        System.out.print("Nota: ");
+        int nota = sc.nextInt();
+
+        System.out.print("Numero: ");
+        int numero = sc.nextInt();
+        sc.nextLine(); //Limpa Buffer (até aqui tem que limpar essa merda)
+
+        return new Anime(nome, comentario, nota, numero);
+    }
+
+    public void mostrarAnime() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Comentário: " + comentario);
+        System.out.println("Nota: " + nota);
+        System.out.println("Número: " + numero);
+        System.out.println();
+        System.out.println("--------------------------------------------");
+        System.out.println();
+    }
+
+    public static String alterarNome(String nome){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Infome o nome: ");
+        nome = sc.nextLine();
+        return nome;
+    }
+
+    public static String alterarComentario(String comentario){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Infome o comentário: ");
+        comentario = sc.nextLine();
+        return comentario;
+    }
+
+    public static int alterarNota(int nota){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Infome a nota: ");
+        nota = sc.nextInt();
+        return nota;
+    }
+
+    public static String removerAcentos(String texto) {
+        return Normalizer.normalize(texto, Normalizer.Form.NFD)
+                .replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.example.Anime.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Anime> listaAnime = new ArrayList<>();
+
+        while (true){
+            System.out.println("1- Cadastrar Anime");
+            System.out.println("2- Pesquisar Anime");
+            System.out.println("3- Alterar Anime");
+            System.out.println("4- Mostrar Lista de Anime");
+            System.out.println("5- Sair");
+
+            int op = sc.nextInt();
+
+            if (op == 1){
+                Anime novoAnime = Anime.cadastrarAnime(sc);
+                listaAnime.add(novoAnime);
+                System.out.println();
+
+            }else if (op == 2){
+                System.out.print("Qual Anime deseja buscar: ");
+                sc.nextLine();
+                String busca = sc.nextLine();
+                System.out.println();
+
+                boolean ver = false;
+
+                for (Anime animes : listaAnime){
+                    String[] palavras = animes.nome.split(" ");
+                    for (String nome : palavras){
+                        if (removerAcentos(nome).equalsIgnoreCase(busca)) {
+                            animes.mostrarAnime();
+                            ver = true;
+                            break;
+                        }
+                    }
+                }
+                if (!ver){
+                    System.out.println("Anime não encontrado");
+                    System.out.println();
+                }
+
+            }else if (op == 3){
+                System.out.print("Qual Anime deseja alterar: ");
+                sc.nextLine();
+                String busca = sc.nextLine();
+                System.out.println();
+
+                boolean ver = false;
+
+                for (Anime animes : listaAnime){
+                    String[] palavras = animes.nome.split(" ");
+                    for (String nome : palavras){
+                        if (removerAcentos(nome).equalsIgnoreCase(busca)) {
+                            System.out.print("Escolha a opção que deseja alterar: ");
+                            System.out.println();
+
+                            System.out.println("1- Nome");
+                            System.out.println("2- Comentário");
+                            System.out.println("3- Nota");
+
+                            String opc = sc.next();
+
+                            if (opc.equalsIgnoreCase("1") || removerAcentos(opc).equalsIgnoreCase("nome")){
+                                animes.nome = alterarNome(animes.nome);
+                                System.out.println();
+                                System.out.println("Nome alterado com sucesso");
+                            } else if (opc.equalsIgnoreCase("2") || removerAcentos(opc).equalsIgnoreCase("comentario")) {
+                                animes.comentario = alterarComentario(animes.comentario);
+                                System.out.println();
+                                System.out.println("Comentário alterado com sucesso");
+                            } else if (opc.equalsIgnoreCase("3") || removerAcentos(opc).equalsIgnoreCase("nota")) {
+                                animes.nota = alterarNota(animes.nota);
+                                System.out.println();
+                                System.out.println("Nota alterada com sucesso");
+                            }else System.out.println("Opção inválida");
+
+                            System.out.println();
+                            animes.mostrarAnime();
+
+                            ver = true;
+                            break;
+                        }
+                    }
+                }
+                if (!ver){
+                    System.out.println("Anime não encontrado");
+                    System.out.println();
+                }
+
+
+            }else if (op == 4){
+                for (Anime mostrar : listaAnime){
+                    mostrar.mostrarAnime();
+                    System.out.println();
+                }
+
+            }else if (op == 5){
+                System.out.println("Saindo...");
+                break;
+            }else System.out.println("Opção invalida");
+        }
+    }
+}
