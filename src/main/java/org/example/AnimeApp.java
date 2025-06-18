4967287f2084a29dd5de177d37a9cb26a73ab6f0
IagoRibLima <iagolima40028922@gmail.com>
2025-06-18 09:01:39 -0300

    remoção do campo numero e iniciando as telas com o Swing

---
+++ src/main/java/org/example/AnimeApp.java
@@ -0,0 +1,41 @@
+/*
+ * Created by JFormDesigner on Tue Jun 17 23:56:17 BRT 2025
+ */
+
+package org.example;
+
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author iago-lima
+ */
+public class AnimeApp extends JFrame {
+    public AnimeApp() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
